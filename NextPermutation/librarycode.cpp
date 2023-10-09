6678305088f2c60c1a9642dc85f2e5cc8dcf9d13
Teenu aswal <73779829+teenu19012287@users.noreply.github.com>
2023-10-09 09:01:01 +0530

    Next_Permutations

---
+++ NextPermutation/librarycode.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> nextPermutation(int N, vector<int> arr){
+        // code here
+        next_permutation(arr.begin() , arr.end());
+        return arr;
+    }
+};
